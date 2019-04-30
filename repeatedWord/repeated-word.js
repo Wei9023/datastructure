75c823c4ad308fd47b73a5409611aeb8e8aa032f
Wei9023 <vv880923@gmail.com>
2019-04-29 23:05:35 -0700

    working

--- repeatedWord/repeated-word.js
+++ repeatedWord/repeated-word.js
@@ -1,17 +1,22 @@
+'use strict';
+
 const {Hashtable} = require('../hash-table/hashtable.js');
 
-const repeatedWord =(string)=>{
-  let ht = new Hashtable();
-  let array = string.split(' ');
-  console.log(array);
-  array.forEach(item => {
-    if(!ht.get(item).includes(item)){
-        ht.add(item,item);
-    }else{
-      return item;
+const repeatedWord = (string) => {
+  if (typeof(string) !== 'string'){
+    return null;
+  }
+  let lowercase = string.toLowerCase();
+  let words = lowercase.split(/[ ?!,.]+/);
+  let dictionary = new Hashtable(1024);
+  for( let i = 0; i < words.length; i++){
+    if( dictionary.get(words[i]) ) {
+      return words[i];
+    } else {
+      dictionary.add(words[i],words);
     }
-  });  
-  
+  }
+  return null;
 };
 
-module.exports={repeatedWord};
+module.exports = {repeatedWord};
